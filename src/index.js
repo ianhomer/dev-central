141f4b221a3470658895d41e3b284cb2d2ba4618
Ian Homer <ian.homer@gmail.com>
2018-06-11 20:59:53 +0100

    Store services in browser storage

--- src/index.js
+++ src/index.js
@@ -3,24 +3,37 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { addService } from './actions'
 import { saga } from './saga'
-import logger from 'redux-logger'
+
+const persistConfig = {
+  key: 'services',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(sagaMiddleware, logger)
 )
 sagaMiddleware.run(saga)
+const persistor = persistStore(store)
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
